819667d6f9b217056255bae8f9c376e6bd522d35
Alex Zyner <a.zyner@acfr.usyd.edu.au>
2018-02-17 13:47:04 +1100

    Added target prio and pixelwise location from centre

--- color_seg.py
+++ color_seg.py
@@ -13,6 +13,7 @@ import time
 #TUNING PARAMS
 erode_iterations = 2
 dilate_iterations = 5
+target_size = 100
 
 cap = cv2.VideoCapture(1)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1920)
@@ -28,7 +29,7 @@ cap.set(cv2.CAP_PROP_FPS, 30)
 # red = cap.get(cv2.CAP_PROP_WHITE_BALANCE_RED_V)
 
 record_images = False
-read_camera = True
+read_camera = False
 
 while (1):
     # Take each frame
@@ -64,50 +65,6 @@ while (1):
         er_mask = cv2.erode(mask, None, iterations=erode_iterations)
         dl_mask = cv2.dilate(er_mask,None,iterations=dilate_iterations)
 
-        # HARD TO TWEAK JUNK, leaving in for now.
-
-        # noise removal
-        # kernel = np.ones((3, 3), np.uint8)
-        # opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=7)
-        #
-        # # sure background area
-        # sure_bg = cv2.dilate(opening, kernel, iterations=3)
-        #
-        # # Finding sure foreground area
-        # dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
-        # ret, sure_fg = cv2.threshold(dist_transform, 0.2 * dist_transform.max(), 255, 0)
-        #
-        # # Finding unknown region
-        # sure_fg = np.uint8(sure_fg)
-        # unknown = cv2.subtract(sure_bg, sure_fg)
-
-        # compute the exact Euclidean distance from every binary
-        # pixel to the nearest zero pixel, then find peaks in this
-        # distance map
-
-
-        #################### Code dump 2 -- WATERSHED
-        # D = ndimage.distance_transform_edt(thresh)
-        # localMax = peak_local_max(D, indices=False, min_distance=20,
-        #                           labels=thresh)
-        #
-        # # perform a connected component analysis on the local peaks,
-        # # using 8-connectivity, then appy the Watershed algorithm
-        # markers = ndimage.label(localMax, structure=np.ones((3, 3)))[0]
-        # labels = watershed(-D, markers, mask=thresh)
-        #
-
-        # ############## BLOB DETECTION
-        # # Set up the detector with default parameters.
-        # params = cv2.SimpleBlobDetector_Params()
-        # params.filterByInertia = False
-        # params.filterByConvexity = False
-        # detector = cv2.SimpleBlobDetector(params)
-        #
-        #
-        import cv2
-
-
         # load the image, convert it to grayscale, blur it slightly,
         # and threshold it
         # find contours in the thresholded image
@@ -116,26 +73,31 @@ while (1):
         cnts = cnts[1]
         image = frame.copy()
         # loop over the contours
+        target_list = []
+
         for c in cnts:
             # compute the center of the contour
             M = cv2.moments(c)
             cX = int(M["m10"] / M["m00"])
             cY = int(M["m01"] / M["m00"])
 
-
             # draw the contour and center of the shape on the image
             cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
             cv2.circle(image, (cX, cY), 7, (255, 255, 255), -1)
-            cv2.putText(image, "center", (cX - 20, cY - 20),
+            if c.size < target_size:
+                cv2.putText(image, "small", (cX - 20, cY - 20),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                target_list.append([c.size,cX])
+            else:
+                cv2.putText(image, "large", (cX - 20, cY - 20),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                target_list.append([c.size, cX])
+        targets = np.array(target_list)
+        largest_target = targets.max(axis=0)
+        target_x = largest_target[1]
 
-        # Detect blobs.
-        #keypoints = detector.detect(dl_mask)
-
-        # Draw detected blobs as red circles.
-        # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
-        #im_with_keypoints = cv2.drawKeypoints(dl_mask, keypoints, np.array([]), (0, 0, 255),
-        #                                      cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        image_width = frame.shape[1]
+        pixel_x_location = target_x - image_width/2
 
         # Show keypoints
         #cv2.imshow("Keypoints", im_with_keypoints)
