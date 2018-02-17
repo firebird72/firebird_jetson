a716dce685bf0af08f65c77551d24dd2aa113ac4
Iain-R <iain@micromelon.com.au>
2018-02-18 05:07:56 +1000

    Added in GPS func

--- gps_utils.py
+++ gps_utils.py
@@ -1,5 +1,25 @@
+
+import serial
+import pynmea2
+import sys
+import math
 class gps_utils:
     def __init__(self):
+                
+        # GPS CONFIG MACROS
+        PMTK_SET_NMEA_BAUDRATE = '$PMTK251,9600*17'
+        PMTK_SET_NMEA_UPDATE_5HZ = "$PMTK220,200*2C"
+        PMTK_SET_NMEA_OUTPUT_RMCONLY = '$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29'
+        PMTK_SET_NMEA_OUTPUT_RMCGGA = "$PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*28"
+        PMTK_SET_NMEA_OUTPUT_GGAONLY = "$PMTK314,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29"
+                # GPS CONFIG ROUTINE
+        serial_gps = serial.Serial()
+        serial_gps.port = '/dev/ttyUSB0'
+        serial_gps.baudrate = 9600
+        serial_gps.open()
+        serial_gps.write(PMTK_SET_NMEA_BAUDRATE + '\r\n')
+        serial_gps.write(PMTK_SET_NMEA_OUTPUT_GGAONLY + '\r\n')
+        serial_gps.write(PMTK_SET_NMEA_UPDATE_5HZ + '\r\n')
         self.waypoint_set = [[-27.8552616667, 153.151291667],
              [-27.8553216667, 153.151405],
              [-27.8554266667, 153.151566667],
@@ -26,7 +46,9 @@ class gps_utils:
         self.destination = None
         self.waypoint = None
 
+    
     def wait_for_gps_lock(self):
+
         return
 
     def update_waypoint(self):
@@ -39,7 +61,31 @@ class gps_utils:
         return
 
     def waypoint_reached(self):
+        
         return
         # check if within X meters of next waypoint. NOT NEXT DESTINATION TARGET.
 
+    def get_gps(NUM_SATS_NEEDED=4):
+    	msg = ""
+    	try:
+    		for line in serial_gps.read():
+    			line = serial_gps.readline()
+    			try: # try statement so that GGAONLY doesn't catch the initial line and crash
+    				msg = pynmea2.parse(line, check=True)
+    			except:
+    				#print('bad line for GGAONLY')
+    				return False, msg
+    			try:
+    				#print("GPS Data: ", msg)
+    				#print(msg.latitude)
+    				#print(msg.longitude)
+    				if int(msg.num_sats) >= NUM_SATS_NEEDED:
+    					return True, msg
+    				else :
+    					return False, msg
+    			except:
+    				#print("bad GPS signal or GGAONLY invoked")
+    				return False, msg
+    	except:
+    		return False, msg
 
