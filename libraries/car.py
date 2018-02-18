b2abee650ecda56dbad93a2467f0d9d00716ea2b
Alex Zyner <a.zyner@acfr.usyd.edu.au>
2018-02-18 12:37:13 +1100

    NAV PULL

---
+++ libraries/car.py
@@ -0,0 +1,47 @@
+import struct
+import serial
+import time
+from settings import CAR_SERIAL_PORT
+
+
+car = serial.Serial()
+car.port = CAR_SERIAL_PORT
+car.baudrate = 9600
+car.open()
+
+Stop=0# 0:false 1:true
+Steer=0
+Acceleration=0
+Brakes=0
+def send():
+    global Stop,Steer,Acceleration,Brakes
+    car.write(struct.pack("<BBBB",Stop,Steer,Acceleration,Brakes ))
+    if Stop:
+        Stop=0
+    pass
+
+def stop():
+    global Stop
+    Stop=1
+    
+
+
+def steer(angle=0):
+    angle=round(angle)
+    angle=min(90,angle)
+    angle=max(-90,angle)
+    global Steer
+    Steer=angle+90
+    
+def acceleration(power=0):#0-100
+    global Acceleration
+    Acceleration=min(max(power,0),1)
+def brakes(power=0):#0-100
+    global Brakes
+    Brakes=power
+if __name__=="__main__":
+    while True:
+        brakes(98)
+        steer(0)
+        send()
+        time.sleep(0.01)
