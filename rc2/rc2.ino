76900522df0c440ad12a095149003349a8688602
Meg <dearmegwhite@gmail.com>
2018-02-18 13:32:28 +1000

    deadman basic

---
+++ rc2/rc2.ino
@@ -0,0 +1,18 @@
+byte PWM_PIN = 3;
+ 
+int pwm_value;
+ 
+void setup() {
+  pinMode(PWM_PIN, INPUT);
+  Serial.begin(115200);
+}
+ 
+void loop() {
+  pwm_value = pulseIn(PWM_PIN, HIGH);
+  Serial.println(pwm_value);
+  
+  if(pwm_value < 1200) {
+  // kill
+  Serial.println("KILL");
+}
+}
