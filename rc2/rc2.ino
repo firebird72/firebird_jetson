639628938ae50a5e1c09cc2bc6c752433ff32d63
Meg <dearmegwhite@gmail.com>
2018-02-18 15:39:14 +1000

    working

--- rc2/rc2.ino
+++ rc2/rc2.ino
@@ -4,16 +4,19 @@ int pwm_value;
  
 void setup() {
   pinMode(PWM_PIN, INPUT);
+  pinMode(7, OUTPUT);
   Serial.begin(115200);
 }
  
 void loop() {
   pwm_value = pulseIn(PWM_PIN, HIGH);
-//  Serial.println(pwm_value);
+  Serial.println(pwm_value);
   
   if(pwm_value < 1200) {
-
-// Serial.println("KILL");
- Serial.println("00000000000000000010000");
+    digitalWrite(7, HIGH);
+    Serial.println(pwm_value);
+    //Serial.println("KILL");
+   //Serial.println("00000000000000000010000");
   }
+ 
 }
